20b0867563821f7725a636fc7db1484ce1257278
Matthew Martin <zhintateo19924559@gmail.com>
2021-10-28 09:46:46 -0500

    Add project files.

---
+++ DevPort/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevPort.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
